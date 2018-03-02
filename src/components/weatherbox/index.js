4a1903bbd69ea7b3671f7fecea7c521eed03c9be
Luke Taylor <luke@lukejt.com>
2018-03-02 14:43:21 +0000

    turn forecast into component

---
+++ src/components/weatherbox/index.js
@@ -0,0 +1,25 @@
+// import preact
+import { h, render, Component } from 'preact';
+
+import style from './style';
+    
+export default class WeatherBox extends Component {
+
+    // rendering a function when the button is clicked
+    render() {
+        return (
+            <div class={ style.test }>
+                <div>
+                    <div>Mon</div>
+                    <div>Tue</div>
+                    <div>Wed</div>
+
+                </div>
+                <div class={ style.today }>
+                    Today
+                </div>
+
+            </div>
+        );
+    }
+}
